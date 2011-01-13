67cab0ab6dd825e9f3793ad3059236ae806600da
koopersmith <koopersmith@d0ae9f73-4e7b-4f95-95dd-514c9059565a>
2011-01-13 03:13:50 +0000

    Tagging 0.4.1. Imported from https://plugins.trac.wordpress.org/changeset/331968/debug-bar

---
+++ tags/0.4.1/debug-bar.dev.js
@@ -0,0 +1,66 @@
+(function(){
+
+var addEvent, preventDefault, toggleQueryList, clickDebugLink;
+
+addEvent = function( obj, type, fn ) {
+	if (obj.addEventListener)
+		obj.addEventListener(type, fn, false);
+	else if (obj.attachEvent)
+		obj.attachEvent('on' + type, function() { return fn.call(obj, window.event);});
+};
+
+preventDefault = function( e ) {
+	// IE doesn't support preventDefault, and does support returnValue
+	if ( e.preventDefault )
+		e.preventDefault();
+	e.returnValue = false;
+}
+
+toggleQueryList = function( e ) {
+	var querylist = document.getElementById( 'querylist' );
+
+	if( querylist && querylist.style.display == 'block' ) {
+		querylist.style.display='none';
+	} else {
+		querylist.style.display='block';
+	}
+
+	preventDefault( e );
+};
+
+clickDebugLink = function( e ) {
+	var sectionDivs, i, j,
+		obj = e.target || e.srcElement;
+
+	if ( ! obj.className || -1 == obj.className.indexOf('debug-menu-link') )
+		return;
+
+	sectionDivs = document.getElementById( 'debug-menu-targets' ).childNodes;
+
+	for ( i = 0; i < sectionDivs.length; i++ ) {
+		if ( 1 != sectionDivs[i].nodeType ) {
+			continue;
+		}
+		sectionDivs[i].style.display = 'none';
+	}
+	document.getElementById( obj.href.substr( obj.href.indexOf( '#' ) + 1 ) ).style.display = 'block';
+
+	for ( j = 0; j < obj.parentNode.parentNode.childNodes.length; j++ ) {
+		if ( 1 != obj.parentNode.parentNode.childNodes[j].nodeType ) {
+			continue;
+		}
+		obj.parentNode.parentNode.childNodes[j].removeAttribute( 'class' );
+	}
+	obj.parentNode.setAttribute( 'class', 'current' );
+
+	preventDefault( e );
+};
+
+addEvent(window, 'load', function() {
+	var adminBarLink = document.getElementById('wp-admin-bar-queries'),
+		adminBarTabs = document.getElementById('debug-menu-links');
+	addEvent( adminBarLink, 'click', toggleQueryList );
+	addEvent( adminBarTabs, 'click', clickDebugLink );
+});
+
+})();
