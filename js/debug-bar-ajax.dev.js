19470764564a58f8988d28181a6f79a116854212
Michael D Adams <mdawaffe@users.noreply.github.com>
2011-01-15 21:32:49 +0000

    Chunk the AJAX Debug into multiple headers.  Decode plus signs correctly Imported from https://plugins.trac.wordpress.org/changeset/333015/debug-bar

--- js/debug-bar-ajax.dev.js
+++ js/debug-bar-ajax.dev.js
@@ -4,17 +4,21 @@ jQuery( function($) {
 	} );
 
 	$(document).ajaxComplete( function( event, xhr, ajaxOptions ) {
-		var html = decodeURIComponent( xhr.getResponseHeader( 'X-Response-Debug' ) ),
+		var html = xhr.getResponseHeader( 'X-Response-Debug' ).split( ', ' ),
 		    span = $( '<span />' ),
 		    head = '<h1>' + span.text( ajaxOptions.type ).html() + ' ' + span.text( ajaxOptions.url ).html() + '</h1><dl>',
-		    data = ajaxOptions.data.split( '&' ),
+		    data = ajaxOptions.data.replace( /\+/, ' ' ).split( '&' ),
 		    i, datum;
+
+		html = $.map( html, function( string ) {
+			return decodeURIComponent( string );
+		} );
 
 		for ( i in data ) {
 			datum = data[i].split( '=' );
 			head += '<dt>' + span.text( decodeURIComponent( datum[0] ) ).html() + '</dt><dd>' + span.text( decodeURIComponent( datum[1] ) ).html() + '</dd>';
 		}
 
-		$( '#debug-menu-target-Debug_Bar_Queries' ).append( head + '</dl>' ).append( html );
+		$( '#debug-menu-target-Debug_Bar_Queries' ).append( head + '</dl>' ).append( html.join( '' ) );
 	} );
 } );
