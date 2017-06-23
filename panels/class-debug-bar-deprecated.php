53c16b617f1b50f33f2d865d66ed8c5b7b341528
jrfnl <jrfnl@users.noreply.github.com>
2017-06-23 04:38:10 +0200

    Minor fixes to the 0.9 release
    * Updated the changelog for all the other changes
    * One changed js script did not have its version number changed, potentially creating issues caused by the browser still serving an old version from the cache.
    * Two strings missing text-domain and translators comment.

--- panels/class-debug-bar-deprecated.php
+++ panels/class-debug-bar-deprecated.php
@@ -129,9 +129,11 @@ class Debug_Bar_Deprecated extends Debug_Bar_Panel {
 		$location = $backtrace[ $bt ]['file'] . ':' . $backtrace[ $bt ]['line'];
 
 		if ( ! is_null( $message ) ) {
-			$message = sprintf( __( '%1$s was called with an argument that is <strong>deprecated</strong> since version %2$s! %3$s' ), $function, $version, $message );
+			/* translators: %1$s is a function name, %2$s a version number, %3$s a message regarding the change. */
+			$message = sprintf( __( '%1$s was called with an argument that is <strong>deprecated</strong> since version %2$s! %3$s', 'debug-bar' ), $function, $version, $message );
 		} else {
-			$message = sprintf( __( '%1$s was called with an argument that is <strong>deprecated</strong> since version %2$s with no alternative available.' ), $function, $version );
+			/* translators: %1$s is a function name, %2$s a version number. */
+			$message = sprintf( __( '%1$s was called with an argument that is <strong>deprecated</strong> since version %2$s with no alternative available.', 'debug-bar' ), $function, $version );
 		}
 
 		$this->deprecated_arguments[ $location ] = array( $message, wp_debug_backtrace_summary( null, $bt ) );
